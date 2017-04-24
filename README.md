c76067962ebc749af4584b2afde5cb2a2dbc91e2
Hannan Ali <abdulhannanali@outlook.com>
2017-04-24 23:40:42 +0500

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# deep-seal
+
+Recursively calls Object.seal on Functions and Objects. Based on the substack's
+[deep-freeze](https://github.com/substack/deep-freeze) module.
---
+++ index.js
@@ -0,0 +1,56 @@
+/**
+ * deep-seal
+ * 
+ * Based on the deep-freeze module provided by the substack.
+ * Seals the object using Object.seal recursively, battle tested
+ * code to include within your repository.
+ */
+'use strict';
+
+// Prevents Error in Strict Mode
+const FUNCTION_PROPERTY_EXCEPTIONS = ['arguments', 'callee', 'caller'];
+
+/**
+ * Recursively calls Object.seal on the Function and Objects and all their 
+ * references
+ * 
+ * @param {Object|Function} o Value to be sealed deeply
+ * @return {Object|Function} Sealed value
+ */
+function deepSeal (o) {
+  const typeO = typeof o;
+  Object.seal(o);
+
+  Object.getOwnPropertyNames(o).forEach(name => {
+    const prop = o[name];
+
+    if (o.hasOwnProperty(name)
+    && prop !== null
+    && typeO === 'object' || typeO === 'function'
+    && !isException(prop, typeO)
+    && !Object.isSealed(prop)) {
+      deepSeal(prop);
+    }
+  });
+
+  return o;
+}
+
+/**
+ * Helper method to determine if the property is an exception
+ * and shouldn't be deeply sealed
+ * 
+ * @param {string} name property name to be check for exception case
+ * @param {string} type type of the parent object/function
+ * 
+ * @return {Boolean} true if it's an exception
+ */
+function isException(prop, type) {
+  if (type === 'function') {
+    return FUNCTION_PROPERTY_EXCEPTIONS.indexOf(prop) !== -1;
+  }
+
+  return true;
+}
+
+module.exports = deepSeal;
